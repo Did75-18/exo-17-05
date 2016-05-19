b974bfcdd3abc63ca1fc20d6c994df0f9af22c7b
Cuper Didier <didier.cuper@gmail.com>
2016-05-19 12:23:35 +0000

    Rectif montant vide

--- avec_bout/calculTva.js
+++ avec_bout/calculTva.js
@@ -7,22 +7,26 @@ var calculez = document.getElementById("calculez");
 
 function calculTtc (){
 	
-	if (tva.value !== "") {
+	if (montant.value == "") {
+		resultat.innerHTML ="Indiquez un montant";
+	}else if(tva.value == ""){
+		resultat.innerHTML ="Indiquez un taux de TVA";
+	}else{
 		var montantTtc = Number(montant.value) + (Number(montant.value) * tva.value)/100;
 		resultat.innerHTML ="Résultat TTC :" + " " + montantTtc.toFixed(2);
-	}else{
-		resultat.innerHTML ="Indiquez un montant de TVA";
+		
 	}
 }
 
 
 function calculHt (){
-	var montantHt;
-	if(tva.value !== ""){
-			var montantHt = Number(montant.value)/(1+(tva.value/100));
-			resultat.innerHTML ="Résultat HT :" + " " + montantHt.toFixed(2);
+	if (montant.value == "") {
+		resultat.innerHTML ="Indiquez un montant";
+	}else if(tva.value == ""){
+		resultat.innerHTML ="Indiquez un taux de TVA";
 	}else{
-		resultat.innerHTML ="Indiquez un montant de TVA";
+		var montantHt = Number(montant.value)/(1+(tva.value/100));
+		resultat.innerHTML ="Résultat HT :" + " " + montantHt.toFixed(2);
 	}
 	
 }
